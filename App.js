015737cb4c08229678b30083d4d42c643dc9e7fc
BrandonHoffey <brandon.hoffey@yahoo.com>
2025-02-14 09:44:49 -0700

    First commit

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Image source={require('./assets/favicon.png')} />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
